87f03fe65abb7094ee4d3bc9e82cf6af72e9c16f
d-pamneja <dpamneja@gmail.com>
2025-03-21 22:52:32 +0530

    Fix : Update Generate Token File

--- examples/react/src/api/generateToken.ts
+++ examples/react/src/api/generateToken.ts
@@ -5,8 +5,13 @@ You can use this token to authenticate the frontend with the Lamatic API.
 */
 
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const access_token = jwt.sign({ project_id: 'your-project-id' }, 'your-secret-key' , {
+// You can set the secret key as an environment variable
+const secretKey = process.env.LAMATIC_SECRET_KEY || 'LamaticSecretKey';
+
+export const access_token = jwt.sign({ project_id: 'your-project-id' }, secretKey , {
     algorithm: 'HS256',
     expiresIn: '1h', // You can set the expiration time as per your requirement
 })
