60a182fed5a24dc6a49f2b967f85462e457db7d7
Naitik Kapadia <88614335+KapadiaNaitik@users.noreply.github.com>
2025-09-10 23:36:53 +0530

    Update API key environment variable for Lamatic

--- examples/react/utils.ts
+++ examples/react/utils.ts
@@ -1,6 +1,7 @@
 import { Lamatic } from 'lamatic'
 
 export const lamaticClient = new Lamatic({
+    endpoint:process.env.LAMATIC_PROJECT_ENDPOINT,
     projectId: process.env.LAMATIC_PROJECT_ID,
-    apiKey: process.env.LAMATIC_API_KEY,
+    apiKey: process.env.LAMATIC_PROJECT_API_KEY,
   })
